beef0bc8c0353314711b267f317e78eba6526aaf
ilkilab04 <ilkilab04@ilki.fr>
2025-03-19 15:33:18 +0100

    multi_thread

--- kali.py
+++ kali.py
@@ -8,19 +8,20 @@ import json
 from datetime import date
 import datetime
 import threading
+import sys
 
 class ffbb:
-    def init(self):
+    def init(self,thr):
         print("init ffbb")
         con=sqlite3.connect("kalisport.sqlite")
         cur=con.cursor()
         cur.execute("delete from ffbb")
         con.commit()
         con.close()
+        
         for champ in self.get_champs():
-            #self.get_data_from_champ(champ)
-            print(champ)
-            threading.Thread(target=self.get_data_from_champ, args=(champ,)).start()
+            thr.append(threading.Thread(target=self.get_data_from_champ, args=(champ,)))
+        
         
     def get_champs(self):
         champs=[]
@@ -46,7 +47,6 @@ class ffbb:
         soup = BeautifulSoup(response.text,"lxml")
         mydivs = soup.find_all("tr", {"class": ["altern-2", "no-altern-2"]})
         for champ in mydivs:
-            print(champ)
             datas = BeautifulSoup(str(champ),"lxml")
             data = datas.find_all("td")
             d=[]
@@ -66,7 +66,7 @@ class ffbb:
         con.close()        
 
 class kali:
-    def init(self):
+    def init(self,thr):
         print("init kali")
         self.teams_href=[]
         con=sqlite3.connect("kalisport.sqlite")
@@ -75,34 +75,44 @@ class kali:
         con.commit()
         con.close()
         self.get_teams()
-        self.get_matchs()
+        for href in self.teams_href:
+            thr.append(threading.Thread(target=self.get_matchs_from_href, args=(href,)))
+
     
-    def results(self):
-        print("\n----------\n results\n----------")
+    def results(self,delta):
+        print("\n----------\n results "+str(delta)+"\n----------")
         con=sqlite3.connect("kalisport.sqlite")
         cur=con.cursor()
         cur.execute("select * from ffbb")
         kali=cur.fetchall()
-        current_week=datetime.datetime.now().isocalendar()[1]
+        current_date=datetime.datetime.now().isocalendar()
+        current_week=str(current_date[1]) if len(str(current_date[1]))==2 else "0"+str(current_date[1])
+        current_week=int(str(current_date[0])+str(current_week))
         for match in kali:
-            week_of_match=datetime.datetime.strptime(match[1], "%d/%m/%Y").isocalendar()[1]
-            if week_of_match==current_week-1:
+            date_of_match=datetime.datetime.strptime(match[1], "%d/%m/%Y").isocalendar()
+            week_of_match=str(date_of_match[1]) if len(str(date_of_match[1]))==2 else "0"+str(date_of_match[1])
+            week_of_match=int(str(date_of_match[0])+str(week_of_match))
+            
+            if week_of_match==current_week+delta:
                 print()
                 cur.execute("select team,ffbb from compet where compet=?",(match[0],))
                 team=cur.fetchone()[0]
                 local=team if "NEUVIL" in match[3] else match[3]
                 away=team if "NEUVIL" in match[4] else match[4]
                 result=re.search("(\d*) - (\d*)",match[5])
-                nba_wins = True if (((result[1] > result[2]) and ("NEUVIL" in match[3])) or ((result[1] < result[2]) and ("NEUVIL" in match[4]))) else False
-                if nba_wins:
-                    print("Victoire des ",end="")
+                if result:
+                    nba_wins = True if (((result[1] > result[2]) and ("NEUVIL" in match[3])) or ((result[1] < result[2]) and ("NEUVIL" in match[4]))) else False
+                    if nba_wins:
+                        print("Victoire des ",end="")
+                    else:
+                        print("Défaite des ",end="")
+                    if "NEUVIL" in match[3]:
+                        print(local+" contre "+away)
+                    else:
+                        print(away+" contre "+local)
+                    print(match[5])
                 else:
-                    print("Défaite des ",end="")
-                if "NEUVIL" in match[3]:
-                    print(local+" contre "+away)
-                else:
-                    print(away+" contre "+local)
-                print(match[5])
+                    print("pas de résultat pour les "+team)    
     
     def next_week(self):
         semaine=["","lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"]
@@ -117,6 +127,7 @@ class kali:
             week_of_match=isodate[1]
             day_of_match=isodate[2]
             if week_of_match==current_week:
+                print(match[0])
                 cur.execute("select team,ffbb from compet where compet=?",(match[0],))
                 team=cur.fetchone()[0]
                 local=team if "NEUVIL" in match[3] else match[3]
@@ -133,45 +144,41 @@ class kali:
         soup = BeautifulSoup(response.text,"lxml")
         
         for ul in soup.find("div",{"id": "content"}):
-            print(ul)
             soup = BeautifulSoup(str(ul),"lxml")
             for a in soup.find_all("a"):
                 if "title" in a.attrs:
                     self.teams_href.append(a.attrs["href"])
         
-    def get_matchs(self):
+    def get_matchs_from_href(self,href):
         con=sqlite3.connect("kalisport.sqlite")
         cur=con.cursor()
-        for href in self.teams_href:
-            print(href)
-            response = requests.get(href)
-            response.encoding = response.apparent_encoding
-            soup = BeautifulSoup(response.text,"lxml")
-            
-            c = soup.find("div",{"class":"bloc-data-equipe"})
-            r = re.search("(<\/div>)([\S\s]*)(<\/div>)",str(c))
-            compet = r.group(2).strip()
-            
-            for jason in soup.find_all("script",{"type":"application/ld+json"}):
-                print(jason)
-                j=json.loads(str(jason.contents[0]))
-                if "@type" in j.keys():
-                    d=re.search("([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2})",j["startDate"])
-                    r=[]
-                    r.append(compet)
-                    r.append(d[3]+"/"+d[2]+"/"+d[1])
-                    x = datetime.datetime(int(d[1]), int(d[2]), int(d[3]))
-                    r.append(d[4]+":"+d[5])
-                    if(self.is_nba_team(j["homeTeam"]["name"])):
-                        r.append(self.get_ffbb_team_name(j["homeTeam"]["name"]))
-                    else:
-                        r.append(j["homeTeam"]["name"])
-                    if(self.is_nba_team(j["awayTeam"]["name"])):
-                        r.append(self.get_ffbb_team_name(j["awayTeam"]["name"]))
-                    else:
-                        r.append(j["awayTeam"]["name"])
-                    if(datetime.datetime.now()<x):
-                        cur.execute("insert into kali values (?,?,?,?,?)",r)
+        response = requests.get(href)
+        response.encoding = response.apparent_encoding
+        soup = BeautifulSoup(response.text,"lxml")
+        
+        c = soup.find("div",{"class":"bloc-data-equipe"})
+        r = re.search("(<\/div>)([\S\s]*)(<\/div>)",str(c))
+        compet = r.group(2).strip()
+        
+        for jason in soup.find_all("script",{"type":"application/ld+json"}):
+            j=json.loads(str(jason.contents[0]))
+            if "@type" in j.keys():
+                d=re.search("([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2})",j["startDate"])
+                r=[]
+                r.append(compet)
+                r.append(d[3]+"/"+d[2]+"/"+d[1])
+                x = datetime.datetime(int(d[1]), int(d[2]), int(d[3]))
+                r.append(d[4]+":"+d[5])
+                if(self.is_nba_team(j["homeTeam"]["name"])):
+                    r.append(self.get_ffbb_team_name(j["homeTeam"]["name"]))
+                else:
+                    r.append(j["homeTeam"]["name"])
+                if(self.is_nba_team(j["awayTeam"]["name"])):
+                    r.append(self.get_ffbb_team_name(j["awayTeam"]["name"]))
+                else:
+                    r.append(j["awayTeam"]["name"])
+                if(datetime.datetime.now()<x):
+                    cur.execute("insert into kali values (?,?,?,?,?)",r)
         con.commit()
         con.close()
         
@@ -208,15 +215,24 @@ def compare():
             print(match_ffbb)
 
 
-
 def main():
+    thr=[]
     f=ffbb()
-    f.init()
+    f.init(thr)
+    
     k=kali()
-    k.init()
-    compare()
-    k.results()
+    k.init(thr)
+    
+    for t in thr:
+        t.start()
+    for t in thr:
+        t.join()
+    
+    k.results(-1)
+    k.results(0)
     k.next_week()
     
+    compare()
+
 if __name__ == "__main__":
     main()
